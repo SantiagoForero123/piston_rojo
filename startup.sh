61f38eda18a58c040674925e79b06f6574a83a96
SantiagoForero123 <galindodavidsantiago520@gmail.com>
2025-06-07 20:55:51 -0500

    Updatesdsd startup.sh

--- startup.sh
+++ startup.sh
@@ -3,10 +3,18 @@ echo "Iniciando despliegue Laravel en Azure..."
 
 cd /home/site/wwwroot || exit
 
+# Permisos necesarios para Laravel
 echo "Dando permisos a storage y bootstrap/cache..."
 chmod -R 775 storage bootstrap/cache
 
+# Cachear configuración y rutas
 echo "Ejecutando optimize y cache config..."
-php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
-echo "Despliegue listo."
+# Ejecutar migraciones forzadas
+echo "Ejecutando migraciones..."
+php artisan migrate --force
+
+echo "Despliegue completado."
